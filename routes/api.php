cf4b4f5bc2de589ad6303dcef71e8a2077496b6e
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-03-24 10:44:07 +0100

    Add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('rooms', [RoomController::class, 'all']);
+Route::get('rooms/{id}', [RoomController::class, 'show']);
+Route::post('rooms', [RoomController::class, 'add']);
+Route::put('rooms/{id}', [RoomController::class, 'edit']);
+Route::delete('rooms/{id}', [RoomController::class, 'delete']);
